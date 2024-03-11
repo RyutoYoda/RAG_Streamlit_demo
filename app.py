df457538afe09ad2e7a543ffff7bb5a929cc5cf2
RyutoYoda <98861847+RyutoYoda@users.noreply.github.com>
2024-03-11 15:42:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,33 +5,29 @@ from sentence_transformers import SentenceTransformer
 import json
 import openai
 
+st.title("RAG with STARBUCKS GPT")
 
-st.title('RAG with STARBUCKS GPT')
+# Streamlitのsecrets機能を使用してAPIキーを取得
+api_key = st.secrets["OPENAI_API_KEY"]
 
-# サイドバーでAPIキーの入力
-api_key = st.sidebar.text_input("Enter your OpenAI API Key:", type="password")
+# 初期設定
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-# セッション状態でメッセージの管理
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
 # すでにあるメッセージをチャット形式で表示
 for message in st.session_state.messages:
-    role = message["role"]
-    content = message["content"]
-    st.chat_message(role, content)
+    st.chat_message(message["role"], message["content"])
 
 # ユーザーからの質問を受け取る
 prompt = st.chat_input("Ask something about Starbucks beverages:")
 
-# ボタンが押されたら処理を実行
-if prompt and api_key:
-    # ユーザーの質問をセッション状態のメッセージに追加
+if prompt:
+    # ユーザーの質問をセッション状態に追加
     st.session_state.messages.append({"role": "user", "content": prompt})
-
-    # OpenAIのAPIキーを設定
-    openai.api_key = api_key
-
+    
     # JSONファイルを読み込む
     file_path = 'starbucks_data.json'
     with open(file_path, 'r') as file:
@@ -49,15 +45,11 @@ if prompt and api_key:
     index = faiss.IndexFlatL2(d)
     index.add(np.array(embeddings).astype('float32'))
 
-    # インデックスをファイルに保存
-    faiss.write_index(index, "faiss_index.bin")
-    # FAISSインデックスを読み込み
-    index = faiss.read_index("faiss_index.bin")
-
-
-    # 質問をベクトル化し、FAISSインデックスを使用して関連する文書を検索
+    # 質問をベクトル化
     question_embedding = model.encode([prompt], convert_to_tensor=True)
     question_embedding = question_embedding.cpu().numpy()
+
+    # 関連する文書を検索
     _, I = index.search(question_embedding, 5)
 
     # 関連する文書の内容を取得
@@ -66,18 +58,20 @@ if prompt and api_key:
     # 関連する文書を基にして、プロンプトを作成
     prompt_for_gpt = prompt + "\n\n" + "\n\n".join(related_docs)
 
-    # OpenAI GPTチャットAPIを使用してテキスト生成
+    # OpenAI GPTを使用してテキスト生成
+    openai.api_key = api_key
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model=st.session_state["openai_model"],
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": prompt_for_gpt}
         ]
     )
 
-    # 生成されたテキストをセッション状態のメッセージに追加
+    # 生成されたテキストをセッション状態に追加
     answer = response.choices[0].message['content']
     st.session_state.messages.append({"role": "assistant", "content": answer})
 
-    # 生成された回答をチャットメッセージとして表示
+    # チャット形式で回答を表示
     st.chat_message("assistant", answer)
+
